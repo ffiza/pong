36ace403eca240ab7bdee105d1ba5e616bada1a1
ffiza <fgiza@protonmail.com>
2022-08-27 10:45:24 -0300

    Update font.

--- code/scoreboard.py
+++ code/scoreboard.py
@@ -14,7 +14,7 @@ class Scoreboard:
         self.settings = Settings()
 
         self.screen = pygame.display.get_surface()
-        self.font = pygame.font.Font(self.settings.font_path,
+        self.font = pygame.font.Font(self.settings.score_font_path,
                                      self.settings.score_font_size)
 
     def draw(self, scores: list) -> None:
